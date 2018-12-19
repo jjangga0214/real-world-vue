f375cb255a4f46f3265ab94e3a57bad354ded0cc
jjangga <jjangga@kookmin.ac.kr>
2018-12-19 09:51:32 +0900

    Revert "Add playing experiment", but lint config
    This reverts commit 01073a4d86e924d835cb184d3e4613694221f38a.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <EventCard></EventCard>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-import EventCard from '@/components/EventCard.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
-    EventCard
+    HelloWorld
   }
 };
 </script>
